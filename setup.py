4ebbb9226cdd5a1c987c0a141888398fba733ee9
Incubadora de Pollos <incubadoradepollos@gmail.com>
2025-09-01 19:05:26 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="anubis_core",
+    version="0.0.2",
+    author="Jose Manuel Herera Saenz",
+    author_email="incubadoradepollos@gmail.com",
+    description="Core for Anubis System",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AnubisSystems/AnubisCore",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Education",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Legal Industry",
+        "Topic :: Education",
+    ]
+    python_requires=">=3.13.0",
+    install_requires=[
+        "requests",
+        "dependency_injector",
+        "pydantic"
+    ],
+)
